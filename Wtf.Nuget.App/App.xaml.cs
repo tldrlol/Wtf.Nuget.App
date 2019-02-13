691ba01484539a4cffc40fff57ad50497e20f204
Daniel Kirillov <tldrlol@protonmail.com>
2019-02-13 20:08:08 +0200

    Initial commit

---
+++ Wtf.Nuget.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Wtf.Nuget.App {
+
+  using System.Windows;
+  using Dep;
+
+  public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+      var c1 = new Class1();
+    }
+  }
+
+}
---
+++ Wtf.Nuget.Dep/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Wtf.Nuget.Dep {
+
+  using System;
+
+  public class Class1 {
+    public readonly object x;
+
+    public Class1() {
+      this.x = HtmlAgilityPack.HtmlDocument.IsWhiteSpace(5);
+    }
+  }
+
+}
